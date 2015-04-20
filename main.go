820d93f0a6e737c40b2b31ccd8877ffb1561306e
Alan Shen <sunzenshen@gmail.com>
2015-04-20 00:59:10 -0600

    Parse and print the AST for a string input

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
-import "github.com/sunzenshen/cgotchas/mpc"
+import (
+	"bufio"
+	"fmt"
+	"github.com/sunzenshen/cgotchas/mpc"
+	"os"
+)
 
 func main() {
+	scanner := bufio.NewScanner(os.Stdin)
 	number := mpc.New("number")
 	operator := mpc.New("operator")
 	expr := mpc.New("expr")
@@ -15,5 +21,15 @@ func main() {
 		"lispy    : /^/ <operator> <expr>+ /$/             ; "
 
 	mpc.MpcaLang(language, number, operator, expr, lispy)
-	mpc.Cleanup(number, operator, expr, lispy)
+	defer mpc.Cleanup(number, operator, expr, lispy)
+
+	for {
+		// Prompt
+		fmt.Print("input> ")
+		// Read a line of user input
+		scanner.Scan()
+		input := scanner.Text()
+		// Print AST using the lispy parser
+		mpc.PrintAst(input, lispy)
+	}
 }
--- mpc/mpc.go
+++ mpc/mpc.go
@@ -7,9 +7,12 @@ package mpc
 import "C"
 
 import (
+	"errors"
 	"unsafe"
 )
 
+type Ast *C.mpc_ast_t
+type Error *C.mpc_err_t
 type Parser *C.mpc_parser_t
 
 func New(name string) Parser {
@@ -27,4 +30,51 @@ func MpcaLang(language string, parsers ...Parser) {
 	defer C.free(unsafe.Pointer(cLanguage))
 	C.mpca_lang_if(C.MPCA_LANG_DEFAULT, cLanguage,
 		parsers[0], parsers[1], parsers[2], parsers[3])
+}
+
+func Parse(input string, parser Parser) (C.mpc_result_t, error) {
+	var r C.mpc_result_t
+	cInput := C.CString(input)
+	defer C.free(unsafe.Pointer(cInput))
+	stdin := C.CString("<stdin>")
+	defer C.free(unsafe.Pointer(stdin))
+	var err error
+	if C.mpc_parse(stdin, cInput, parser, &r) == C.int(0) {
+		err = errors.New("mpc: failed to parse input string")
+	}
+	return r, err
+}
+
+func GetOutput(result *C.mpc_result_t) Ast {
+	cast := (*Ast)(unsafe.Pointer(result))
+	return *cast
+}
+
+func GetError(result *C.mpc_result_t) Error {
+	cast := (*Error)(unsafe.Pointer(result))
+	return *cast
+}
+
+func ErrDelete(result *C.mpc_result_t) {
+	C.mpc_err_delete(GetError(result))
+}
+
+func ErrPrint(result *C.mpc_result_t) {
+	C.mpc_err_print(GetError(result))
+}
+
+func AstDelete(result *C.mpc_result_t) {
+	C.mpc_ast_delete(GetOutput(result))
+}
+
+// PrintAst parses an input string for its AST representation
+func PrintAst(input string, parser Parser) {
+	r, err := Parse(input, parser)
+	if err != nil {
+		ErrPrint(&r)
+		ErrDelete(&r)
+	} else {
+		C.mpc_ast_print(GetOutput(&r))
+		AstDelete(&r)
+	}
 }
